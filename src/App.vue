693e6ad15c3e9fe9dd13106430e283396beaf7fb
Teerawat6204101320 <teerawatloawang@gmail.com>
2021-03-05 18:21:33 +0700

    uppdate

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/"></router-link> 
+      <router-link to="/about"></router-link>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style>
+*{
+   margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
 }
-
 #nav {
   padding: 30px;
 }
